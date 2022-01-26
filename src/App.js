0d5c245d0e0092c6f68ae5dc4d9c93d1b03d6b26
Xuanning Huang <xnhh@Xuannings-MacBook-Air.local>
2022-01-26 14:19:40 +0800

    fix:库版本补全

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { useSnackbar } from 'notistack';
 
-function App() {
+function App () {
+  const { enqueueSnackbar } = useSnackbar();
+  const handleClick = event => {
+    event.preventDefault();
+    enqueueSnackbar("This is a message",{variant:"success"});
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +25,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button style={{fontSize:"25px",color:"white",margin:"10px"}} onClick={handleClick}>
+          Click Me
+        </Button>
       </header>
     </div>
   );
